e152b10a34c183cf67751ea3885d05bcf86931c0
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-03 05:41:39 -0600

    Pantalla Inicial

---
+++ app/src/main/java/com/example/elizabethcakes/InicialActivity.kt
@@ -0,0 +1,28 @@
+package com.example.elizabethcakes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class InicialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicial2)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@InicialActivity,MainActivity::class.java))
+                finish()
+            },
+            2500
+
+        )
+    }
+}
