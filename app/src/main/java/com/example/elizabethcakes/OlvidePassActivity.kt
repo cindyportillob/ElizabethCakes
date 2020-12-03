245b26248c42d6ab4984997b35f90cb9f60e6eb0
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-03 03:46:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/elizabethcakes/OlvidePassActivity.kt
@@ -0,0 +1,11 @@
+package com.example.elizabethcakes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OlvidePassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_olvide_pass)
+    }
+}
