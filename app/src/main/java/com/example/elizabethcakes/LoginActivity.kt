139a46919a255f25e2fc3f04c471c3f161afe366
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-03 11:38:53 -0600

    Pantalla Login

--- app/src/main/java/com/example/elizabethcakes/LoginActivity.kt
+++ app/src/main/java/com/example/elizabethcakes/LoginActivity.kt
@@ -1,26 +1,21 @@
 package com.example.elizabethcakes
 
-import android.os.Build
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.WindowInsets
-import android.view.WindowManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
-class LoginActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-        //esconder la barra de status
-        @Suppress("DEPRECATION")
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        }else{
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
     }
 }
