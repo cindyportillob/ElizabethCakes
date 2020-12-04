67966d3562dbdccbc85362ac91c2e307c15f169e
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-04 05:10:10 -0600

    Conexion Firestore

--- app/src/main/java/com/example/elizabethcakes/Firestore/FirestoreClass.kt
+++
@@ -1,8 +0,0 @@
-package com.example.elizabethcakes.Firestore
-
-import com.google.firebase.firestore.FirebaseFirestore
-
-class FirestoreClass {
-
-        private val EFireStore = FirebaseFirestore.getInstance()
-}
