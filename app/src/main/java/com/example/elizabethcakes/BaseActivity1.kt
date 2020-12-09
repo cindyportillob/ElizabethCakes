c9c215045ccd610b0620abeb0b7c7d077b995cab
gabyed <gestrada318@gmail.com>
2020-12-09 00:07:36 -0600

    registro

---
+++ app/src/main/java/com/example/elizabethcakes/BaseActivity1.kt
@@ -0,0 +1,37 @@
+package com.example.elizabethcakes
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import kotlin.Boolean as Boolean1
+
+open class BaseActivity1: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base1)
+    }
+    fun showErrorSnackBar(message: String, errorMessage: Boolean1) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+
+        if (errorMessage){
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity1,
+                    R.color.colorSnackBarError
+                )
+            )
+        }else{
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity1,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+
+        }
+        snackBar.show()
+    }
+}
--- app/src/main/java/com/example/elizabethcakes/Registro.kt
+++ app/src/main/java/com/example/elizabethcakes/Registro.kt
@@ -2,19 +2,18 @@ package com.example.elizabethcakes
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_login.tv_registrar
 import kotlinx.android.synthetic.main.activity_registro.*
 
-class Registro : AppCompatActivity() {
+class Registro : BaseActivity1() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
-//SIUUUUU
+
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -30,6 +29,45 @@ class Registro : AppCompatActivity() {
             startActivity(Intent(this@Registro, LoginActivity::class.java))
             finish()
         }
+
+        button2.setOnClickListener{
+            validateRegisterDetails()
+        }
+    }
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(nombre_completo.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_nombre_completo),
+                    true
+                )
+                false
+            }
+            TextUtils.isEmpty(email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_email),
+                    true
+                )
+                false
+            }
+            TextUtils.isEmpty(Password1.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_Password1),
+                    true
+                )
+                false
+            }
+            Password1.text.toString().trim { it <= ' ' } != Password2.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_Password2), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.registery_successfull), false)
+                true
+            }
+        }
     }
 
 }
+
