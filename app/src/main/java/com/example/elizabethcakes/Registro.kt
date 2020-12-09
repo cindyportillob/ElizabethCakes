582196488492c84c60620e8074d28d7c5d3aebbd
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-09 10:34:37 -0600

    Firebase Auth

--- app/src/main/java/com/example/elizabethcakes/Registro.kt
+++ app/src/main/java/com/example/elizabethcakes/Registro.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.tv_registrar
 import kotlinx.android.synthetic.main.activity_registro.*
 
@@ -66,6 +70,38 @@ class Registro : BaseActivity1() {
                 showErrorSnackBar(resources.getString(R.string.registery_successfull), false)
                 true
             }
+        }
+    }
+
+    private fun registerUser() {
+
+        // Check with validate function if the entries are valid or not.
+        if (validateRegisterDetails()) {
+
+
+
+            val email: String = email.text.toString().trim { it <= ' ' }
+            val password: String = Password1.text.toString().trim { it <= ' ' }
+
+            // Create an instance and create a register a user with email and password.
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+
+                        if (task.isSuccessful) {
+
+                            // Firebase registered user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                            showErrorSnackBar(
+                                "You are registered successfully. Your user id is ${firebaseUser.uid}",
+                                false
+                            )
+                        } else {
+                           
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    })
         }
     }
 
